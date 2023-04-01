1754fee01791190701b7812ee375b233e2ac2001
gogones <aag.annas@gmail.com>
2023-04-01 08:47:54 +0700

    add rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    // federation({
+    // name: "remote_app",
+    // filename: "sspa-main.js",
+    // exposes: {
+    //   './Button': './src/components/Button'
+    // },
+    // shared: ['react','react-dom']
+  // })
+  ],
+  build: {
+    // modulePreload: false,
+    // target: 'esnext',
+    // minify: false,
+    // cssCodeSplit: false,
+    rollupOptions: {
+      input: 'src/sspa-main.jsx',
+      format: 'system',
+      preserveEntrySignatures: true
+    },
+  }
 })
