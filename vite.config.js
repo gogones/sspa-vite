5f8c3bf456bcbb3f95f108e119c9a085b711422b
gogones <aag.annas@gmail.com>
2023-04-01 15:02:03 +0700

    add dynamic base url from env

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,39 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 // import federation from '@originjs/vite-plugin-federation'
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    // federation({
-    // name: "remote_app",
-    // filename: "sspa-main.js",
-    // exposes: {
-    //   './Button': './src/components/Button'
-    // },
-    // shared: ['react','react-dom']
-  // })
-  ],
-  // base: import.meta.env.BASE_URL,
-  build: {
-    // modulePreload: false,
-    // target: 'esnext',
-    // minify: false,
-    // cssCodeSplit: false,
+export default defineConfig(({mode}) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react(),
+      // federation({
+      // name: "remote_app",
+      // filename: "sspa-main.js",
+      // exposes: {
+      //   './Button': './src/components/Button'
+      // },
+      // shared: ['react','react-dom']
+      // })
+    ],
+    base: env.BASE_URL,
+    build: {
+      // modulePreload: false,
+      // target: 'esnext',
+      // minify: false,
+      // cssCodeSplit: false,
 
-    rollupOptions: {
-      input: 'src/sspa-main.jsx',
-      format: 'system',
-      preserveEntrySignatures: true,
-      output: {
-        entryFileNames: 'sspa-main.js',
-      }
-    },
+      rollupOptions: {
+        input: 'src/sspa-main.jsx',
+        format: 'system',
+        preserveEntrySignatures: true,
+        output: {
+          entryFileNames: 'sspa-main.js',
+        }
+      },
+    }
   }
 })
