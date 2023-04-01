fe268d49240bec0b9180678b5ca23e8a3245ce95
gogones <aag.annas@gmail.com>
2023-04-01 15:04:24 +0700

    disable cssCodeSplit

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig(({mode}) => {
       // modulePreload: false,
       // target: 'esnext',
       // minify: false,
-      // cssCodeSplit: false,
+      cssCodeSplit: false,
 
       rollupOptions: {
         input: 'src/sspa-main.jsx',
